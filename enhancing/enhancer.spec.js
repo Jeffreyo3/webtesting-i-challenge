0202764adeb25acf17e85175761770f024876454
Jeffrey Orndorff <jeffrey.orndorff@gmail.com>
2020-03-25 15:38:06 -0700

    Add additional conditional testing to enhancer repair function

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -6,8 +6,13 @@ describe("Enhancer functions succeed", () => {
         it("Restores item durability to 100", () => {
             const item = {
                 name: "Big Sword",
-                durability: 1,
+                durability: 0,
                 enhancement: 0
+            }
+            const item2 = {
+                name: "Flame Bow",
+                durability: 55,
+                enhancement: 20
             }
             const expectedItem = {
                 name: "Big Sword",
@@ -15,10 +20,16 @@ describe("Enhancer functions succeed", () => {
                 enhancement: 0
             }
 
-        
+            // Check that item is repaired to expectedItem
             expect(enhancer.repair(item)).toEqual(expectedItem)
-            expect(enhancer.repair(item).durability).toStrictEqual(expectedItem.durability)
+
+            // Check that durability is repaired even when parameter object contains other key value pairs
+            expect(enhancer.repair(item2)).not.toStrictEqual(expectedItem)
+            expect(enhancer.repair(item2).durability).toStrictEqual(expectedItem.durability)
+
+            // Check that item is returned as a new object with a durability rating above 0
             expect(enhancer.repair(item)).toBeTruthy()
+            expect(enhancer.repair(item).durability).toBeTruthy()
         })
     })
 
