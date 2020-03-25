caf4337c6eb67d243ed421f117da94b53d01c942
Jeffrey Orndorff <jeffrey.orndorff@gmail.com>
2020-03-25 15:27:40 -0700

    Initialize project
    Initialize npm, add jest dependency
    
    Write enhancer functions
    
    Testenhancer.repair()

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -5,16 +5,36 @@ module.exports = {
   get,
 };
 
+/* item = {
+  name: "Big Sword",
+  enhancement: 20,
+  durability: 100
+}  */
+
 function succeed(item) {
-  return { ...item };
+
+  if (item.enhancement < 20) {
+    return { ...item, enhancement: item.enhancement + 1 };
+  } else {
+    return { ...item };
+  };
 }
 
 function fail(item) {
-  return { ...item };
+
+  if (item.enhancement < 15) {
+    return { ...item, enhancement: item.enhancement - 5 };
+  } else if (item.enhancement === 15 || item.enhancement === 16) {
+    return { ...item, enhancement: item.enhancement - 10 };
+  } else if (item.enhancement < 16) {
+    return { ...item, enhancement: item.enhancement - 1 };
+  } else {
+    return { ...item };
+  }
 }
 
 function repair(item) {
-  return { ...item };
+  return { ...item, durability: 100 };
 }
 
 function get(item) {
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,26 @@
 const enhancer = require('./enhancer.js');
 // test away!
+
+describe("Enhancer functions succeed", () => {
+    describe("Repair Item Successful", () => {
+        it("Restores item durability to 100", () => {
+            const item = {
+                name: "Big Sword",
+                durability: 1,
+                enhancement: 0
+            }
+            const expectedItem = {
+                name: "Big Sword",
+                durability: 100,
+                enhancement: 0
+            }
+
+        
+            expect(enhancer.repair(item)).toEqual(expectedItem)
+            expect(enhancer.repair(item).durability).toStrictEqual(expectedItem.durability)
+            expect(enhancer.repair(item)).toBeTruthy()
+        })
+    })
+
+
+}) 
