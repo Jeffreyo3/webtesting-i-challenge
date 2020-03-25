c0ca4f8d628d70471e04f06b58a4e295af5dd5f9
Jeffrey Orndorff <jeffrey.orndorff@gmail.com>
2020-03-25 15:56:03 -0700

    Refactor repair tests to contain multiple it() checks
    Add enhancement successful tests

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,37 +1,65 @@
 const enhancer = require('./enhancer.js');
 // test away!
 
+const item = {
+    name: "Big Sword",
+    durability: 0,
+    enhancement: 0
+}
+const item2 = {
+    name: "Flame Bow",
+    durability: 55,
+    enhancement: 0
+}
+
 describe("Enhancer functions succeed", () => {
-    describe("Repair Item Successful", () => {
-        it("Restores item durability to 100", () => {
-            const item = {
-                name: "Big Sword",
-                durability: 0,
-                enhancement: 0
-            }
-            const item2 = {
-                name: "Flame Bow",
-                durability: 55,
-                enhancement: 20
-            }
-            const expectedItem = {
-                name: "Big Sword",
-                durability: 100,
-                enhancement: 0
-            }
+    describe("*REPAIR ITEM DURABILITY FUNCTIONS ARE SUCCESSFUL*", () => {
 
-            // Check that item is repaired to expectedItem
+        const expectedItem = {
+            name: "Big Sword",
+            durability: 100,
+            enhancement: 0
+        }
+
+        it("Check that item is repaired to 100 durability", () => {
             expect(enhancer.repair(item)).toEqual(expectedItem)
+        })
 
-            // Check that durability is repaired even when parameter object contains other key value pairs
+        it("Check that durability is repaired even when parameter object contains other key value pairs", () => {
             expect(enhancer.repair(item2)).not.toStrictEqual(expectedItem)
             expect(enhancer.repair(item2).durability).toStrictEqual(expectedItem.durability)
+        })
 
-            // Check that item is returned as a new object with a durability rating above 0
+        it("Check that item is returned as a new object with a durability rating above 0", () => {
             expect(enhancer.repair(item)).toBeTruthy()
             expect(enhancer.repair(item).durability).toBeTruthy()
         })
     })
 
+
+    describe("*ENHANCEMENT SUCCEED FUNCTIONS ARE SUCCESSFUL*", () => {
+
+        const expectedItem = {
+            name: "Big Sword",
+            durability: 0,
+            enhancement: 1
+        }
+
+        it("Adds +1 to enhancement on success", () => {
+            expect(enhancer.succeed(item).enhancement).toEqual(item.enhancement + 1)
+            expect(enhancer.succeed(item)).toEqual(expectedItem)
+        })
+
+        it("Check that enhancement is increased even when parameter object contains other key value pairs", () => {
+            expect(enhancer.succeed(item2)).not.toStrictEqual(expectedItem)
+            expect(enhancer.succeed(item2).enhancement).toStrictEqual(expectedItem.enhancement)
+        })
+
+        it("Check that item is returned as a new object with a enhancement rating above 0", () => {
+            expect(enhancer.succeed(item)).toBeTruthy()
+            expect(enhancer.succeed(item).enhancement).toBeTruthy()
+        })
+
+    })
 
 }) 
